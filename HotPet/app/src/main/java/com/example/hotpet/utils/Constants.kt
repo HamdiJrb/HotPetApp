55f1f5f4041a0c0a06adfc4b2f165afd750d3963
HamdiJrb <hamdi.jerbi@esprit.tn>
2022-12-31 12:27:42 +0100

    url

--- HotPet/app/src/main/java/com/example/hotpet/utils/Constants.kt
+++ HotPet/app/src/main/java/com/example/hotpet/utils/Constants.kt
@@ -3,7 +3,8 @@ package com.example.hotpet.utils
 object Constants {
     private const val PORT = "5000"
 
-    const val BASE_URL = "http://192.168.1.20:$PORT/"
+    //const val BASE_URL = "http://192.168.1.17:$PORT/"
+    const val BASE_URL = "https://hotpetserver.up.railway.app/"
 
     const val BASE_URL_IMAGES = BASE_URL + "images-files/"
 
